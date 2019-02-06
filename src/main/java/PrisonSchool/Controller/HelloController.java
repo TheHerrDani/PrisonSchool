80433001e905742d47512a019b55fb1b84391b79
kustox <gyorgy.vargadudas@gmail.com>
2019-02-06 23:21:26 +0100

    Add request mapping to controller and Get mapping to method level

--- src/main/java/PrisonSchool/Controller/HelloController.java
+++ src/main/java/PrisonSchool/Controller/HelloController.java
@@ -1,14 +1,16 @@
 package PrisonSchool.Controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/Hello")
 public class HelloController {
 
     @CrossOrigin(origins = "http://localhost:8080")
-    @RequestMapping("/Main")
+    @GetMapping("/Main")
     public String index() {
         return "We all worship dai kami sama";
     }
