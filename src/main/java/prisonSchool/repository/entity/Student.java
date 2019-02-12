762189f435dc523f028d02277b89646c344658d2
TheHerrDani <Sipos.Herr.Dani@gmail.com>
2019-02-12 19:36:35 +0100

    add properties for the Student class

--- src/main/java/prisonSchool/repository/entity/Student.java
+++ src/main/java/prisonSchool/repository/entity/Student.java
@@ -1,4 +1,31 @@
 package prisonSchool.repository.entity;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer productId;
+
+    @Length(min = 1)
+    private String name;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
